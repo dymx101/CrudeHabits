788f5ab5cdf4d9c7f8a5215f146a6456a47e4569
Daniel Dong <ydong@gagein.com>
2014-04-22 15:32:14 +0800

    tuing

--- Source/CrudeHabits/CrudeHabits/GNGameVC.m
+++ Source/CrudeHabits/CrudeHabits/GNGameVC.m
@@ -28,6 +28,7 @@ typedef enum {
     UIImageView             *_ivPlayPause;
     UIImageView             *_ivClose;
     UIView                  *_viewPauseBlock;
+    UIView                  *_viewPausePanel;
     
     GNVertProgressView      *_viewTeam1Progress;
     UIView                  *_viewTeam1ProgressBG;
@@ -59,9 +60,65 @@ typedef enum {
 @property (nonatomic, strong)     UIButton    *btnPlayAgain;
 @property (nonatomic, strong)     UIButton    *btnCategory;
 
+@property (nonatomic, strong)     UILabel     *lblQuitMessage;
+@property (nonatomic, strong)     UIButton    *btnQuit;
+@property (nonatomic, strong)     UIButton    *btnResume;
+
 @end
 
 @implementation GNGameVC
+
+- (void)installPauseView
+{
+    ////
+    
+    _viewPauseBlock = [UIView new];
+    _viewPauseBlock.backgroundColor = [UIColor colorWithWhite:1 alpha:.5];
+    [self.view addSubview:_viewPauseBlock];
+    [_viewPauseBlock alignToView:self.view];
+    _viewPauseBlock.hidden = YES;
+    
+    _viewPausePanel = [UIView new];
+    _viewPausePanel.backgroundColor = [FDColor sharedInstance].themeRed;
+    _viewPausePanel.layer.cornerRadius = 10.f;
+    [_viewPauseBlock addSubview:_viewPausePanel];
+    [_viewPausePanel constrainWidth:@"220" height:@"220"];
+    [_viewPausePanel alignCenterWithView:_viewPauseBlock];
+    _viewPausePanel.hidden = YES;
+    
+    _lblQuitMessage = [UILabel new];
+    _lblQuitMessage.font = [UIFont fontWithName:FONT_REGULAR size:20];
+    _lblQuitMessage.textColor = [UIColor whiteColor];
+    _lblQuitMessage.numberOfLines = 0;
+    _lblQuitMessage.textAlignment = NSTextAlignmentCenter;
+    _lblQuitMessage.text = @"Are you sure you want to quit?";
+    [_viewPausePanel addSubview:_lblQuitMessage];
+    [_lblQuitMessage alignLeading:@"10" trailing:@"-10" toView:_viewPausePanel];
+    [_lblQuitMessage alignTopEdgeWithView:_viewPausePanel predicate:@"20"];
+    
+    _btnQuit = [UIButton new];
+    _btnQuit.backgroundColor = [UIColor whiteColor];
+    _btnQuit.titleLabel.font = [UIFont fontWithName:FONT_REGULAR size:25];//[UIFont boldSystemFontOfSize:30];
+    [_btnQuit setTitleColor:[FDColor sharedInstance].themeRed forState:UIControlStateNormal];
+    [_btnQuit setTitle:@"Yes" forState:UIControlStateNormal];
+    _btnQuit.layer.cornerRadius = 10.f;
+    [_viewPausePanel addSubview:_btnQuit];
+    [_btnQuit constrainWidth:@"150" height:@"45"];
+    [_btnQuit alignCenterXWithView:_viewPausePanel predicate:@"0"];
+    [_btnQuit constrainTopSpaceToView:_lblQuitMessage predicate:@"20"];
+    
+    
+    _btnResume = [UIButton new];
+    _btnResume.backgroundColor = [UIColor whiteColor];
+    _btnResume.titleLabel.font = [UIFont fontWithName:FONT_REGULAR size:25];//[UIFont boldSystemFontOfSize:30];
+    [_btnResume setTitleColor:[FDColor sharedInstance].themeRed forState:UIControlStateNormal];
+    [_btnResume setTitle:@"No" forState:UIControlStateNormal];
+    _btnResume.layer.cornerRadius = 10.f;
+    [_viewPausePanel addSubview:_btnResume];
+    [_btnResume constrainWidth:@"150" height:@"45"];
+    [_btnResume alignCenterXWithView:_viewPausePanel predicate:@"0"];
+    [_btnResume constrainTopSpaceToView:_btnQuit predicate:@"10"];
+}
 
 - (void)viewDidLoad
 {
@@ -87,14 +144,10 @@ typedef enum {
     [self.view addConstraint:constraint];
     
     
+    [self installPauseView];
+    
+    
     ////
-    
-    _viewPauseBlock = [UIView new];
-    _viewPauseBlock.backgroundColor = [UIColor colorWithWhite:1 alpha:.5];
-    [self.view addSubview:_viewPauseBlock];
-    [_viewPauseBlock alignToView:self.view];
-    _viewPauseBlock.hidden = YES;
-    
     _ivPlayPause = [UIImageView new];
     _ivPlayPause.image = [UIImage imageNamed:@"pause"];
     [self.view addSubview:_ivPlayPause];
@@ -114,7 +167,7 @@ typedef enum {
     [_ivClose alignLeadingEdgeWithView:self.view predicate:@"10"];
     [_ivClose constrainWidth:@"26" height:@"26"];
     _ivClose.userInteractionEnabled = YES;
-    UITapGestureRecognizer *tapClose = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(playPauseAction)];
+    UITapGestureRecognizer *tapClose = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(quitGameAction)];
     [_ivClose addGestureRecognizer:tapClose];
     
     
@@ -269,6 +322,9 @@ typedef enum {
     [_btnNext addTarget:self action:@selector(nextWordAction) forControlEvents:UIControlEventTouchUpInside];
     [_btnPlayAgain addTarget:self action:@selector(playAgainAction) forControlEvents:UIControlEventTouchUpInside];
     [_btnCategory addTarget:self action:@selector(categoriesAction) forControlEvents:UIControlEventTouchUpInside];
+    
+    [_btnQuit addTarget:self action:@selector(doQuit) forControlEvents:UIControlEventTouchUpInside];
+    [_btnResume addTarget:self action:@selector(doResume) forControlEvents:UIControlEventTouchUpInside];
     
     
     ////
@@ -452,9 +508,32 @@ typedef enum {
     [self.presentingViewController dismissViewControllerAnimated:YES completion:nil];
 }
 
+-(void)quitGameAction {
+    [self pauseAction];
+    _viewPausePanel.hidden = NO;
+    _ivPlayPause.userInteractionEnabled = NO;
+}
+
+-(void)doQuit {
+    
+    UIViewController *presentingVC = self.presentingViewController;
+    [presentingVC dismissViewControllerAnimated:NO completion:^{
+        [presentingVC.presentingViewController dismissViewControllerAnimated:NO completion:nil];
+    }];
+}
+
+-(void)doResume {
+    [self resumeAction];
+    _ivPlayPause.userInteractionEnabled = YES;
+}
+
 -(void)playPauseAction {
-    _isGamePaused = !_isGamePaused;
-    _isGamePaused ? [self pauseAction] : [self resumeAction];
+    
+    if (_isGamePaused) {
+        [self resumeAction];
+    } else {
+        [self pauseAction];
+    }
     
     if (_isGamePaused) {
         if (_isPlayingQuickTick) {
@@ -471,12 +550,17 @@ typedef enum {
     [_timer setFireDate:[NSDate distantFuture]];
     _ivPlayPause.image = [UIImage imageNamed:@"play"];
     _viewPauseBlock.hidden = NO;
+    _viewPausePanel.hidden = YES;
+    
+    _isGamePaused = YES;
 }
 
 -(void)resumeAction {
     [_timer setFireDate:[NSDate date]];
     _ivPlayPause.image = [UIImage imageNamed:@"pause"];
     _viewPauseBlock.hidden = YES;
+    
+    _isGamePaused = NO;
 }
 
 -(void)tapTeam1Action:(id)sender {
